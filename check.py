1c1a2cd2330f7157232b1ba89a07e2fe6b6f435c
mazumdarsoubhik <mazumdarsoubhik@gmail.com>
2020-12-08 22:34:28 +0530

    pc

--- check.py
+++ check.py
@@ -1,6 +1,20 @@
-per = [125,125,2,8,5645,654,545,15,54,54]
-per_sum = sum(per)
-for x in range(len(per)):
-    temp = (float)per[x]
-    per[x] = round((per[x]/per_sum)*100)
-print(per)
+def to_dict_api(percentages,categories,top_keywords,frame,i): #frame and i to get id
+    mydict = {}
+    cat_array =[]
+    empty_percent = [0]*4
+    percent_array = [y for y in percentages]
+    percent_array.extend(empty_percent)
+    mydict['user_id'] = frame['id'].iloc[i]
+    mydict['keywords'] = json.dumps(top_keywords.tolist())
+    for i in range(len(categories)):
+        cat_array.append({'tag':categories[i],'percentage':percent_array[i]})
+    mydict['categories'] = json.dumps(cat_array)
+    return mydict
+
+
+frame = pd.DataFrame(columns = ['id','url'], data = [[123,'f.com'],[345,'q.com']])
+percent = [5,8,7,9]
+cat = ['alpha','beta','coma','delta']
+tops = ['hello', 'ali', 'bhatt', 'wikis', 'enabled', 'wiki', 'access', 'control', 'rules', 'may', 'imposed', 'organize', 'content']
+file = to_dict_api(percent,cat,tops,frame,1)
+print(file)
